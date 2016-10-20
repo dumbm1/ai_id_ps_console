c275916b53a1d87195a520207426f7f4c806ee7c
Marat Shagiev <m_js@bk.ru>
2016-10-21 02:04:23 +0300

    add themes; begin to add list with themes

--- js/main.js
+++ js/main.js
@@ -1,75 +1,89 @@
 /*jslint vars: true, plusplus: true, devel: true, nomen: true, regexp: true, indent: 4, maxerr: 50 */
 /*global $, window, location, CSInterface, SystemPath, themeManager*/
 
-(function () {
+(function() {
   'use strict';
 
-  var csInterface = new CSInterface ();
+  var csInterface = new CSInterface();
 
-  function loadJSX (fileName) {
-    var extensionRoot = csInterface.getSystemPath (SystemPath.EXTENSION) + "/jsx/";
-    csInterface.evalScript ('$.evalFile("' + extensionRoot + fileName + '")');
+  function loadJSX(fileName) {
+    var extensionRoot = csInterface.getSystemPath(SystemPath.EXTENSION) + "/jsx/";
+    csInterface.evalScript('$.evalFile("' + extensionRoot + fileName + '")');
   }
 
-  var editor = ace.edit ("editor");
-  editor.setTheme ("ace/theme/github");
-  editor.getSession ().setMode ("ace/mode/javascript");
+  function init() {
 
-  function init () {
+    themeManager.init();
+    loadJSX("json2.js");
 
-    themeManager.init ();
-    loadJSX ("json2.js");
+    var aceThemes = ['ambiance', 'chaos', 'chrome', 'clouds', 'clouds_midnight', 'cobalt', 'crimson_editor', 'dawn',
+      'dreamweaver', 'eclipse', 'github', 'idle-fingers', 'iplastic', 'katzenmilch', 'ke_theme', 'kuroir', 'merbivore',
+      'merbivore_soft', 'mono_industrial', 'monokai', 'pastel_on_dark', 'solarized_light', 'solarized_dark', 'sqlserver',
+      'terminal', 'textmate', 'tomorrow', 'tomorrow_night', 'tomorrow_night_blue', 'tomorrow_night_bright',
+      'tomorrow_night_eighties', 'twilight', 'vibrant_ink', 'xcode'];
 
-    $ ("#editor").keyup (function (e) {
+    for (var i = 0; i < aceThemes.length; i++) {
+      var obj = aceThemes[i];
+
+    }
+
+
+    var editor = ace.edit("editor");
+    editor.setTheme("ace/theme/" + $("#themes").val());
+    editor.getSession().setMode("ace/mode/javascript");
+
+
+
+    $("#editor").keyup(function(e) {
       if (e.ctrlKey && e.keyCode == 13) {
-        var fld_return = document.getElementById ('fld_return');
+        var fld_return = document.getElementById('fld_return');
         if (!fld_return.value) {
-          fld_return.value     = evalInChrome (editor.getValue ());
+          fld_return.value     = evalInChrome(editor.getValue());
           fld_return.scrollTop = fld_return.scrollHeight - fld_return.clientHeight;
         } else {
-          fld_return.value += '\n' + evalInChrome (editor.getValue ());
+          fld_return.value += '\n' + evalInChrome(editor.getValue());
           fld_return.scrollTop = fld_return.scrollHeight - fld_return.clientHeight;
         }
-        editor.focus ();
+        editor.focus();
       } else if ((e.metaKey || e.altKey) && e.keyCode == 13) {
-        var elem_return = document.getElementById ('fld_return');
-        evalInAi (editor.getValue (), elem_return);
-        editor.focus ();
+        var elem_return = document.getElementById('fld_return');
+        evalInAi(editor.getValue(), elem_return);
+        editor.focus();
       }
     })
 
-    $ ("#btn_eval_js").click (function () {
-      var fld_return = document.getElementById ('fld_return');
+    $("#btn_eval_js").click(function() {
+      var fld_return = document.getElementById('fld_return');
       if (!fld_return.value) {
-        fld_return.value     = evalInChrome (editor.getValue ());
+        fld_return.value     = evalInChrome(editor.getValue());
         fld_return.scrollTop = fld_return.scrollHeight - fld_return.clientHeight;
       } else {
-        fld_return.value += '\n' + evalInChrome (editor.getValue ());
+        fld_return.value += '\n' + evalInChrome(editor.getValue());
         fld_return.scrollTop = fld_return.scrollHeight - fld_return.clientHeight;
       }
-      editor.focus ();
+      editor.focus();
     });
-    $ ("#btn_eval_jsx").click (function () {
-      var elem_return = document.getElementById ('fld_return');
-      evalInAi (editor.getValue (), elem_return);
-      editor.focus ();
+    $("#btn_eval_jsx").click(function() {
+      var elem_return = document.getElementById('fld_return');
+      evalInAi(editor.getValue(), elem_return);
+      editor.focus();
     });
-    $ ("#btn_clear").click (function () {
-      $ ("#fld_return").val ('');
+    $("#btn_clear").click(function() {
+      $("#fld_return").val('');
     });
 
-    $ ("#btn_refrash").click (reloadPanel);
-    $ ("#btn_close").click (function () {
+    $("#btn_refrash").click(reloadPanel);
+    $("#btn_close").click(function() {
       /*csInterface.evalScript ("killCEP()");*/
-      new CSInterface ().closeExtension ();
+      new CSInterface().closeExtension();
     });
 
   }
 
-  init ();
+  init();
 
-  function reloadPanel () {
-    location.reload ();
+  function reloadPanel() {
+    location.reload();
   }
 
   /**
@@ -78,10 +92,10 @@
    * @param {String} str - the javascript code string
    * @return {String} res - the evaluation result or error message
    * */
-  function evalInChrome (str) {
+  function evalInChrome(str) {
     var res = '';
     try {
-      res = eval (str);
+      res = eval(str);
     } catch (e) {
       res = e/*.message + ', ' + e.line*/;
     }
@@ -93,8 +107,8 @@
    *
    * @param {String} str - the javascript code string
    * */
-  function evalInAi (str, fld_return) {
-    csInterface.evalScript ('evalStr(' + JSON.stringify (str) + ')', function (res) {
+  function evalInAi(str, fld_return) {
+    csInterface.evalScript('evalStr(' + JSON.stringify(str) + ')', function(res) {
       if (!fld_return.value) {
         fld_return.value     = '[ai]: ' + res;
         fld_return.scrollTop = fld_return.scrollHeight - fld_return.clientHeight;
@@ -104,4 +118,4 @@
       }
     });
   }
-} ());
+}());
