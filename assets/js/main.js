4e8011f1516935b229d9ecb2c2d60b1341a7db3f
Marat Shagiev <21020173+dumbm1@users.noreply.github.com>
2024-02-23 22:09:05 +0300

    Added native Ace-editor settings menu, some minor (patch) changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,49 +17,132 @@
   /*  if (localStorage.output_field) {
    document.getElementById("output_field").className = localStorage.output_field;
    }*/
-  if (localStorage.editorCut) {
+/*  if (localStorage.editorCut) {
    document.getElementById('editor').className = localStorage.editorCut;
-  }
+  }*/
+  
 
-  let is_autocomplete = document.getElementById('is_autocomplete');
+  // let is_autocomplete = document.getElementById('is_autocomplete');
   // let fldOutput = document.getElementById('output_field');
+  let btn_settings = document.getElementById('btn_settings');
 
-  let aceThemes = getAceThemes();
-  insertThemesList(aceThemes);
+  let ace_settingsmenu;
+
+  let aceMenuOpts;
+  if (localStorage.aceMenuOpts) {
+   aceMenuOpts = localStorage.aceMenuOpts;
+  }
+  
+
+/*  let aceThemes = getAceThemes();
+  insertThemesList(aceThemes);*/
+
+
+
+  let editor = ace.edit("editor");
 
   ace.require("ace/ext/language_tools");
-
-  var editor = ace.edit("editor");
-  editor.setTheme("ace/theme/" + $("#themes").val());
-  editor.getSession().setMode("ace/mode/javascript");
-  editor.setOptions({
-                     enableBasicAutocompletion: is_autocomplete.checked,
-                     enableSnippets: false,
-                     enableLiveAutocompletion: is_autocomplete.checked,
-                    });
-
-  if (localStorage.fntSize) {
-   document.getElementById('editor').style.fontSize = localStorage.fntSize;
+  ace.require('ace/ext/settings_menu').init(editor);
+  
+  // editor.setOptions(JSON.parse(localStorage.aceMenuOpts));
+  if(aceMenuOpts) {
+   editor.setOptions(JSON.parse(aceMenuOpts));
   }
 
-  $("#themes").change(function () {
-   var editor = ace.edit("editor");
+
+  // editor.setTheme("ace/theme/" + $("#themes").val());
+  // editor.getSession().setMode("ace/mode/javascript");
+  editor.setOptions({
+                     enableBasicAutocompletion: true,
+                     /*                     enableBasicAutocompletion: is_autocomplete.checked,
+                      enableSnippets: false,
+                      enableLiveAutocompletion: is_autocomplete.checked,*/
+                    });
+
+  btn_settings.addEventListener('click', (e) => {
+   editor.showSettingsMenu();
+   
+   ace_settingsmenu = document.getElementById('ace_settingsmenu');
+
+   console.log(`Clock to settings button == ${e.target == btn_settings};\nAce settings menu is open;\nMenu element below:`);
+   console.log(ace_settingsmenu);
+   
+   ace_settingsmenu.style.fontSize = '12px';
+   ace_settingsmenu.querySelectorAll('input[type=number]').forEach((elem)=>{
+    elem.style.width = '4rem';
+   })
+
+   setTimeout(() => {
+    document.documentElement.addEventListener('click', (e) => {
+     if (!e.target.contains(ace_settingsmenu)) return;
+
+     aceMenuOpts = editor.getOptions();
+     localStorage.aceMenuOpts = JSON.stringify(aceMenuOpts);
+
+/*     console.log(`Click outside of Ace menu == ${e.target.contains(ace_settingsmenu)};\nAce settings menu is closed.`);
+     console.log(editor.getOptions());*/
+    });
+   }, 300);
+
+  });
+
+  /*  document.documentElement.addEventListener('click', (e) => {
+   /!*   if (e.target != btn_settings) {
+   console.log(e.target);
+
+   // console.log('Меню закрыто');
+   }*!/
+
+   if (e.target.contains(document.getElementById('ace_settingsmenu'))) {
+   console.log('Меню закрыто');
+   }
+   });*/
+
+  /*  btn_settings.addEventListener('click', (e) => {
+   console.log('Клик на кнопке settings');
+
+   if (isSettingsMenuOpen === false) {
+   editor.showSettingsMenu();
+   isSettingsMenuOpen = true;
+   console.log('Меню появиилось, пользователь выставляет настройки');
+
+   setTimeout(() => {
+   document.addEventListener('click', (e) => {
+   if (isSettingsMenuOpen === true && e.target != document.getElementById('ace_settingsmenu')) {
+   console.log('Клик на документе при открытом меню');
+   isSettingsMenuOpen = false;
+   console.log('Меню закрылось');
+   console.log('Взяли настройки - let aceEditorOpts = editor.getOptions()');
+   console.log('Далее надо записать актуальные настройки в LocalStorage');
+   console.log('Чтобы потом во время следующей сессии установить актуальные настройки из LocalStorage - editor.setOptions(aceEditorOpts)');
+   // document.removeEventListener('click');
+   }
+   }, 1000);
+   });
+   }
+
+   // alert(JSON.stringify(options));
+   // alert(options.enableLiveAutocompletion);
+   });*/
+
+/*  $("#themes").change(function () {
+   let editor = ace.edit("editor");
    editor.setTheme("ace/theme/" + $("#themes").val());
    editor.getSession().setMode("ace/mode/javascript");
    localStorage.theme = $("#themes").val();
-  });
+  });*/
 
-  is_autocomplete.addEventListener('change', (e) => {
+/*  is_autocomplete.addEventListener('change', (e) => {
    editor.setOptions({
                       enableBasicAutocompletion: is_autocomplete.checked,
                       enableSnippets: false,
                       enableLiveAutocompletion: is_autocomplete.checked,
                      });
-  });
+  });*/
 
   $("#editor").keyup(function (e) {
    if (e.ctrlKey && e.keyCode == 13) {
-    var fld_return = document.getElementById('output_field');
+    let fld_return = document.getElementById('output_field');
     if (!fld_return.value) {
      fld_return.value = evalInChrome(editor.getValue());
      fld_return.scrollTop = fld_return.scrollHeight - fld_return.clientHeight;
@@ -69,14 +152,14 @@
     }
     editor.focus();
    } else if ((e.metaKey || e.altKey) && e.keyCode == 13) {
-    var elem_return = document.getElementById('output_field');
+    let elem_return = document.getElementById('output_field');
     evalInAi(editor.getValue(), elem_return);
     editor.focus();
    }
   });
 
   $("#btn_eval_js").click(function () {
-   var fld_return = document.getElementById('output_field');
+   let fld_return = document.getElementById('output_field');
    if (!fld_return.value) {
     fld_return.value = evalInChrome(editor.getValue());
     fld_return.scrollTop = fld_return.scrollHeight - fld_return.clientHeight;
@@ -88,7 +171,7 @@
   });
 
   $("#btn_eval_jsx").click(function () {
-   var elem_return = document.getElementById('output_field');
+   let elem_return = document.getElementById('output_field');
    evalInAi(editor.getValue(), elem_return);
    editor.focus();
   });
@@ -102,7 +185,7 @@
   });
 
   $("#btn_close").click(function () {
-   var apiVersion = csInterface.getCurrentApiVersion();
+   let apiVersion = csInterface.getCurrentApiVersion();
    if (apiVersion.major > 6) {
     csInterface.requestOpenExtension('js_console_dialog');
     csInterface.closeExtension();
@@ -118,7 +201,7 @@
  }
 
  function loadJSX(fileName) {
-  var extensionRoot = csInterface.getSystemPath(SystemPath.EXTENSION) + "/jsx/";
+  let extensionRoot = csInterface.getSystemPath(SystemPath.EXTENSION) + "/jsx/";
   csInterface.evalScript('$.evalFile("' + extensionRoot + fileName + '")');
  }
 
@@ -129,7 +212,7 @@
   * @return {String} res - the evaluation result or error message
   * */
  function evalInChrome(str) {
-  var res = '';
+  let res = '';
   try {
    res = eval(str);
   } catch (e) {
@@ -158,10 +241,10 @@
  /**
   * @param {Array}
   * */
- function insertThemesList(themesList) {
+/* function insertThemesList(themesList) {
   for (var i = 0; i < themesList.length; i++) {
-   var theme = themesList[i];
-   var optTheme = document.createElement('option');
+   let theme = themesList[i];
+   let optTheme = document.createElement('option');
    optTheme.innerHTML = theme;
    if (localStorage.theme == theme) {
     optTheme.selected = true;
@@ -170,9 +253,9 @@
    }
    document.getElementById("themes").appendChild(optTheme);
   }
- }
+ }*/
 
- function getAceThemes() {
+/* function getAceThemes() {
   return [
    'ambiance',
    'chaos', 'chrome', 'cloud9_day', 'cloud9_night', 'clouds_midnight', 'cloud9_night_low_color', 'cloud_editor',
@@ -190,5 +273,5 @@
    'terminal', 'textmate', 'tomorrow', 'tomorrow_night', 'tomorrow_night_blue', 'tomorrow_night_bright', 'tomorrow_night_eighties', 'twilight',
    'vibrant_ink',
    'xcode'];
- }
+ }*/
 }());
